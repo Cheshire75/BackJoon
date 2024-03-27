f6b639df2d0394360413327a8b48f05e9dd48e71
Cheshire75 <jhr75468@gmail.com>
2024-03-27 17:08:50 +0900

    새싹

---
+++ 25083.cpp
@@ -0,0 +1,24 @@
+// 새싹
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "         ,r\'\"7 \n"
+              << "r`-_   ,\'  ,/\n"
+              << " \\. \". L_r\'\n"
+              << "   `~\\/\n"
+              << "      |\n"
+              << "      |\n"
+              << std::endl;
+    return 0;
+}
